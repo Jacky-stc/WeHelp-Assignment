08362647bfe4822256e1e8838cd703f07696a8b3
Jacky-stc <112391673+Jacky-stc@users.noreply.github.com>
2022-10-05 07:06:20 +0800

    Update open_data.py

--- week-3/open_data.py
+++ week-3/open_data.py
@@ -12,13 +12,6 @@ with open("data.csv", "w", encoding="utf-8") as file:
             file.write(place["stitle"]+","+place["address"][5:8]+","+ 
             place["longitude"]+","+place["latitude"]+","+place["file"][0:end+3]+"\n")
 
-# 為什麼這樣不行？
-# with open("data.txt", "w", encoding="utf-8") as file:
-#     for place in viewlist:
-#         if "2014" not in place["xpostDate"] or "2013" not in place["xpostDate"]:
-#             file.write(place["stitle"]+","+place["address"][5:8]+","+ 
-#             place["longitude"]+","+place["latitude"]+","+place["xpostDate"]+"\n")
-# ans:因為當成是執行到"2014"是否不在字串中時，回傳值為true，就會直接執行下面的程式，不會再確認"2013"是否也不在字串中
-# 
+
 # 還沒解決要怎麼同時查找兩個子字串的問題
 # end=place["file"].find("jpg") 
