ab582b5f00470595d5c1ae182b379117d3058d38
Jacky-stc <112391673+Jacky-stc@users.noreply.github.com>
2022-10-25 20:07:10 +0800

    Update app.py

--- week-6/app.py
+++ week-6/app.py
@@ -4,7 +4,7 @@ import pymysql
 db = pymysql.connect(
     host = 'localhost',
     user = 'root',
-    password = 'jacky13257',
+    password = '',
     database = 'website',
     charset = 'utf8'
 ) # 與mysql資料庫連接並設定參數
