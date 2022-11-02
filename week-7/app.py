ff82d948acce097c796e36755f25410da7b7f91e
Jacky Su <stzuchieh@gmail.com>
2022-11-02 18:53:49 +0800

    first version

---
+++ week-7/app.py
@@ -0,0 +1,169 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+import mysql.connector
+import os
+
+dbconfig = {
+    "host" : "localhost",
+    "user" : "root",
+    "password" : "13579jacky",
+    "database" : "website"
+}
+
+connection_pool = mysql.connector.pooling.MySQLConnectionPool(
+    pool_name = "my_pool",
+    pool_size = 5,
+    pool_reset_session = True,
+    **dbconfig
+)
+
+app = Flask(
+    __name__,
+    static_folder = "static", 
+    static_url_path = "/" 
+)
+app.secret_key = os.urandom(20)
+
+@app.route("/", methods=['POST', 'GET'])
+def homepage():
+    return render_template("homepage.html")
+
+@app.route("/signup", methods=['POST','GET'])
+def signup():
+    account = request.form["account"]
+    try:
+        connection_object = connection_pool.get_connection()
+        cursor =  connection_object.cursor()
+        check_sql = "SELECT * FROM member WHERE username = %s"
+        check_val = account
+        cursor.execute(check_sql,check_val)
+        user = cursor.fetchone()
+        connection_object.commit()
+        user == None
+        name = request.form["name"]
+        account = request.form["account"]
+        password = request.form["password"]
+        signup_sql = "INSERT INTO member (name, username, password) VALUES (%s,%s,%s)"
+        signup_val = (name, account, password)
+        cursor.execute(signup_sql, signup_val)
+        connection_object.commit()
+        return redirect(url_for("homepage"))
+    except:
+        return redirect(url_for("error", message = "帳號已經被註冊"))
+    finally:
+        cursor.close()
+        connection_object.close()
+
+@app.route("/signin",methods=['GET','POST'])
+def signin():
+    account = request.form["account"]
+    password = request.form["password"]
+    try:
+        connection_object = connection_pool.get_connection()
+        cursor =  connection_object.cursor()
+        signin_sql = "SELECT * FROM member WHERE username = %s and password = %s"
+        signin_val = (account,password)
+        cursor.execute(signin_sql,signin_val)
+        user = cursor.fetchone()
+        connection_object.commit()
+        user != None
+        session["id"] = user[0]
+        session["name"] = user[1]
+        session["state"] = "已登入"
+        return redirect(url_for("member"))
+    except:
+        return redirect(url_for("error", message="帳號或密碼輸入錯誤"))
+    finally:
+        cursor.close()
+        connection_object.close()
+        
+        
+
+@app.route("/member", methods=['GET', 'POST'])
+def member():
+    try:
+        connection_object = connection_pool.get_connection()
+        cursor =  connection_object.cursor()
+        session["state"] == "已登入"
+        cursor.execute("SELECT member.name, message.content, message.time FROM message INNER JOIN member ON member.id = message.member_id ORDER BY time")
+        info = cursor.fetchall()
+        return render_template("member.html", name=session["name"],content = info)
+    except:
+        return redirect(url_for("homepage"))
+    finally:
+        cursor.close()
+        connection_object.close()
+        
+@app.route("/api/member", methods=['GET'])
+def searching_name():
+    username = request.args.get("username", "")
+    try:
+        connection_object = connection_pool.get_connection()
+        cursor =  connection_object.cursor()
+        sql = "SELECT * FROM member WHERE username = %s"
+        val = (username, )
+        cursor.execute(sql, val)
+        user = cursor.fetchone()
+        connection_object.commit
+        user != None
+        return {
+            "data":{
+            "id":user[0],
+            "name":user[1],
+            "username":user[2]
+            }
+        }
+    except:
+        return {
+            "data":None
+        }
+    finally:
+        cursor.close()
+        connection_object.close()
+
+@app.route("/api/member", methods = ['PATCH'])
+def change_info():
+    req = request.get_json()
+    try:
+        connection_object = connection_pool.get_connection()
+        cursor =  connection_object.cursor()
+        sql  = "UPDATE member SET name = %s WHERE id = %s"
+        val = (req["name"], 7)
+        cursor.execute(sql, val)
+        connection_object.commit()
+        session["name"]=req["name"]
+        return {"ok":True}
+    except:
+        return {"error":True}
+    finally:
+        cursor.close()
+        connection_object.close()
+    
+
+@app.route("/message",methods = ['POST'])
+def message():
+    content = request.form["content"]
+    try:
+        connection_object = connection_pool.get_connection()
+        cursor =  connection_object.cursor()
+        message_sql = "INSERT INTO message (member_id, content) VALUES (%s,%s)"
+        message_val = (session['id'],content)
+        cursor.execute(message_sql, message_val)
+        connection_object.commit()
+        return redirect(url_for("member"))
+    finally:
+        cursor.close()
+        connection_object.close()
+    
+
+@app.route("/error")
+def error():
+    message = request.args.get("message","")
+    return render_template("error.html", alert=message) 
+
+@app.route("/signout")
+def signout():
+    session.clear() 
+    return redirect(url_for("homepage"))
+
+if __name__ == "__main__":
+    app.run(port="3000", debug=True)
---
+++ week-7/static/fetch.js
@@ -0,0 +1,50 @@
+// 查詢會員
+const result = document.getElementById("search_form");
+let search_name = document.createElement("div");
+let username;
+function get_name(){
+    let user_account = search_form.username.value.toString();
+    let url =  "http://127.0.0.1:3000/api/member?username=" + user_account ;
+    fetch(url, {method: "GET"})
+    .then((response) =>{
+        return response.json();
+    })
+    .then((data) =>{
+        if (data.data == null){
+            search_name.textContent =  "查無此用戶"
+        }else{
+            username = data.data['name'];
+            search_name.textContent = username + "(" + user_account + ")";
+        }
+        result.appendChild(search_name);
+        document.getElementById("name").value = "";
+    })
+}
+// 更改姓名
+const updated = document.getElementById("alter_form")
+let update_message = document.createElement("div")
+function alter_name(){
+    let content = {"name": alter_form.alter.value.toString()};
+    fetch("http://127.0.0.1:3000/api/member", {
+        method: 'PATCH',
+        headers: {
+            'Content-type' :'application/json; charset=UTF-8',
+            'Accept':'application/jason'
+        },
+        body: JSON.stringify(content)
+    }).then((response) =>{
+        return response.json();
+    }).then((data) =>{
+        if(data['ok']== true){
+            update_message.textContent = "更新成功"
+        }else{
+            update_message.textContent = "更新失敗"
+        }
+        update_message.setAttribute("id", "congrat")
+        updated.appendChild(update_message)
+        document.getElementById("alter").value = "";
+        setTimeout(() => {
+            updated.removeChild(update_message)
+        }, 5000);
+    })
+}
